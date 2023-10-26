691289bf4e8da706be782c00e5476d0f874f0e4f
IP98765 <144829395+IP98765@users.noreply.github.com>
2023-10-26 21:06:41 +0000

    add html and images

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, IP!</p>"
+    return render_template("home.html")
 # app was created above, but run 
 # until the if statement
 if __name__ == "__main__":
